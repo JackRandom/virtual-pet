4f0631c0680f03632540b74e86f09ee9973d8062
JackRandom <andrewglogan1987@gmail.com>
2020-08-20 19:45:26 +0100

    pets can get older, hungrier and less fit

--- src/pet.js
+++ src/pet.js
@@ -1,3 +1,6 @@
+const fido = new Pet('Fido');
+const rex = new Pet('Rex');
+
 const pet = {};
 
 pet.name = 'Fido';
@@ -5,7 +8,16 @@ pet.name = 'Fido';
 
 
 function Pet(name) {
-    this.name = name;
-  };
+  this.name = name;
+  this.age = 0;
+  this.hunger = 0;
+  this.fitness = 10;
+}
+
+Pet.prototype.growUp = function() {
+  this.age += 1;
+  this.hunger += 5;
+  this.fitness -= 3;
+};
 
 module.exports = Pet;
--- tests/pet.test.js
+++ tests/pet.test.js
@@ -15,3 +15,40 @@ describe('constructor', () => {
     expect(pet.name).toEqual('Fido');
   });
 });
+
+describe('constructor', () => {
+  
+
+  it('has a initial age of 0', () => {
+    const pet = new Pet('Fido');
+
+    expect(pet.age).toEqual(0);
+  });
+});
+describe('growUp', () => {
+  it('increments the age by 1', () => {
+    const pet = new Pet('Fido');
+
+    pet.growUp();
+
+    expect(pet.age).toEqual(1);
+  });
+});
+describe('growUp hunger', () => {
+  it('increments the age by 1 and adds 5 hunger to the pets hunger number', () => {
+    const pet = new Pet('Fido');
+
+    pet.growUp();
+
+    expect(pet.hunger).toEqual(5);
+  });
+});
+describe('growUp fitness', () => {
+  it('increments the age by 1,adds 5 hunger to the pets hunger number and takes 3 off the pets total fitness', () => {
+    const pet = new Pet('Fido');
+
+    pet.growUp();
+
+    expect(pet.fitness).toEqual(7);
+  });
+});
